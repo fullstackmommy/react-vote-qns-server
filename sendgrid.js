f0d77ad12fed3890eef86787551a76a984bb7c72
Wong Pi Wei and Nicole Budiman <wpwei_16+nicole.budiman@yahoo.com>
2019-04-20 20:57:12 +0800

    Testing sendgrid

---
+++ sendgrid.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MAIL_API_KEY2: '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'
+}
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const {sequelize} = require("./models")
 const {createEventsAndQuestions, createUsers} = require('./seed')
 const port = process.env.PORT || 8080
 const eraseDatabaseOnSync = true
+const {MAIL_API_KEY2} = require('./sendgrid')
+
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(MAIL_API_KEY2);
 
 sequelize
     .sync({force: eraseDatabaseOnSync})
@@ -15,6 +21,13 @@ sequelize
                 console.log(`Server is running on Heroku with port number ${port}`);
             } else {
                 console.log(`Server is running on http://localhost:${port}`)
+                const msg = {
+                    to: 'nicole.budiman@gmail.com',
+                    from: 'nicole.budiman@gmail.com',
+                    subject: 'Project Assignment',
+                    text: 'Sending an email to assign you to a new project'
+                };
+                sgMail.send(msg);
             }
         })
     })
